6a25031afafec188994e08ce3499558a051d27fc
samno1607 <swig.sg@googlemail.com>
2013-01-26 01:27:22 +0000

    Increase policy->min To 307.2Mhz To Decrease Some Lag

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -487,7 +487,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
-	policy->min = 115200;
+	policy->min = 307200;
 	policy->max = 1228800;
 	policy->cur = omap_getspeed(policy->cpu);
 

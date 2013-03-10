f52ae7f1d058d9ce52329b6629d5f21974982652
samno1607 <swig.sg@googlemail.com>
2013-03-10 18:29:50 +0000

    Update Smart Reflex To Calibrate High Frequencies

--- arch/arm/mach-omap2/smartreflex-class1p5.c
+++ arch/arm/mach-omap2/smartreflex-class1p5.c
@@ -436,6 +436,15 @@ static int sr_class1p5_enable(struct voltagedomain *voltdm,
 	if (volt_data->volt_calibrated)
 		return 0;
 
+	/* Based on Imoseyon's idea to properly calibrate high frequencies e.g. >= 1.4Ghz MPU */
+	if (volt_data->volt_nominal >= 1375000) {
+		volt_data->volt_calibrated = volt_data->volt_nominal;
+		volt_data->volt_dynamic_nominal = volt_data->volt_nominal;
+		pr_info("[franciscofranco] %p - nominal %d", __func__, volt_data->volt_nominal);
+		pr_info("[franciscofranco] %p - in the selected OPP its default nominal voltage is equal or above 1375mV so we don't calibrate it to prevent a crash.", __func__);
+		return 0;
+	}
+
 	work_data = (struct sr_class1p5_work_data *)voltdm_cdata;
 	if (IS_ERR_OR_NULL(work_data)) {
 		pr_err("%s: bad work data??\n", __func__);
